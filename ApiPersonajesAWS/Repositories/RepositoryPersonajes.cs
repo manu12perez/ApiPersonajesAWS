115e42dbb9fc2945e0cd7d632140d4139b9cf09f
manu12perez <manu12perez@gmail.com>
2025-05-13 11:11:41 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Repositories/RepositoryPersonajes.cs
@@ -0,0 +1,37 @@
+﻿using ApiPersonajesAWS.Data;
+using ApiPersonajesAWS.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiPersonajesAWS.Repositories
+{
+    public class RepositoryPersonajes
+    {
+        private PersonajesContext context;
+
+        public RepositoryPersonajes(PersonajesContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Personaje>> GetPersonajesAsync()
+        {
+            return await this.context.Personajes.ToListAsync();
+        }
+
+        private async Task<int> GetMaxIdPersonajesAsync()
+        {
+            return await this.context.Personajes.MaxAsync(x => x.IdPersonaje) + 1;
+        }
+
+        public async Task CreatePersonajeAsync(string nombre, string imagen)
+        {
+            Personaje personaje = new Personaje();
+            personaje.IdPersonaje = await this.GetMaxIdPersonajesAsync();
+            personaje.Nombre = nombre;
+            personaje.Imagen = imagen;
+
+            await this.context.Personajes.AddAsync(personaje);
+            await this.context.SaveChangesAsync();
+        }
+    }
+}
